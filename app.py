e9837e4add5d26277d2fd0cc5ab2f91db4884d96
Abhinav260804 <abhinav260804@gmail.com>
2023-04-07 10:55:29 +0530

    files of html created

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,request
 from model import *
-from model1 import *
 
-
+#creating the app
 app=Flask(__name__)
 
-
+#configuring the app
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///agriculturedata.sqlite3"
 
-db.init_app(app)
-app.app_context().push()
+@app.route('/',methods=['GET',"POST"])
+def index_page():
+    return render_template('index.html')
+
+@app.route('/crop_rec',methods=['GET','POST'])
+def crop_recommendation():
+    return render_template('recommendation.html')
+
+@app.route('/foreign_crop',methods=['GET','POST'])
+def foreign_crop():
+    return render_template('foreign.html')
+
+@app.route('/price',methods=['GET','POST'])
+def price_prediction():
+    return render_template('price_prediction.html')
+
