d52a06a2dfda1c4f747c42d200b908efb8604d0f
SaiSatya16 <jj1039@srmist.edu.in>
2023-04-07 11:12:16 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template,redirect,request
 from model import *
-
+import numpy as np
+import pandas as pd
+import pickle
+from ml_model import *
+crop_recommendation_model_path = 'knn_pipeline.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
 #creating the app
 app=Flask(__name__)
 
@@ -13,7 +19,15 @@ def index_page():
 
 @app.route('/crop_rec',methods=['GET','POST'])
 def crop_recommendation():
-    return render_template('recommendation.html')
+    if request.method == 'POST':
+        f = [float(x) for x in request.form.values()]
+        data = [np.array(f)]
+        my_prediction = crop_recommendation_model.predict(data)
+        final_prediction = my_prediction[0]
+        final_prediction = label_dict[final_prediction]
+
+        return render_template('recommendation.html', prediction_text=final_prediction,)
+
 
 @app.route('/foreign_crop',methods=['GET','POST'])
 def foreign_crop():
@@ -22,4 +36,7 @@ def foreign_crop():
 @app.route('/price',methods=['GET','POST'])
 def price_prediction():
     return render_template('price_prediction.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
