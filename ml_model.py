f24b847c9b652ffe31383ec22af9dc129796280a
SaiSatya16 <jj1039@srmist.edu.in>
2023-04-07 10:56:37 +0530

    ml-model-updated

--- ml_model.py
+++ ml_model.py
@@ -11,7 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import  KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.metrics import accuracy_score, plot_confusion_matrix, confusion_matrix
+from sklearn.metrics import accuracy_score
 import pickle
 
 import warnings
